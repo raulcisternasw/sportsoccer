43696cf8bcbf158c2916eeec9f9e0fa7e9c21658
Raul Cisternas <r.cisternas.w@gmail.com>
2017-06-19 04:31:44 -0400

    UPDATE: Controller soccerfield

--- app/controllers/soccerfields_controller.rb
+++ app/controllers/soccerfields_controller.rb
@@ -1,5 +1,11 @@
 class SoccerfieldsController < ApplicationController
   before_action :set_soccerfield, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:new, :edit]
+  before_action :set_code, only: [:new, :edit]
+
+  def set_code
+    @code = ['CF001', 'CF002', 'CF003', 'CF004', 'CF005', 'CF006', 'CF007', 'CF008', 'CF009', 'CF010']
+  end  
 
   # GET /soccerfields
   # GET /soccerfields.json
@@ -14,7 +20,7 @@ class SoccerfieldsController < ApplicationController
 
   # GET /soccerfields/new
   def new
-    @soccerfield = Soccerfield.new
+    @soccerfield = Soccerfield.new    
   end
 
   # GET /soccerfields/1/edit
@@ -25,10 +31,11 @@ class SoccerfieldsController < ApplicationController
   # POST /soccerfields.json
   def create
     @soccerfield = Soccerfield.new(soccerfield_params)
+    @soccerfield.state = 1
 
     respond_to do |format|
       if @soccerfield.save
-        format.html { redirect_to @soccerfield, notice: 'Soccerfield was successfully created.' }
+        format.html { redirect_to @soccerfield, notice: 'Reserva creada con éxito.' }
         format.json { render :show, status: :created, location: @soccerfield }
       else
         format.html { render :new }
@@ -42,7 +49,7 @@ class SoccerfieldsController < ApplicationController
   def update
     respond_to do |format|
       if @soccerfield.update(soccerfield_params)
-        format.html { redirect_to @soccerfield, notice: 'Soccerfield was successfully updated.' }
+        format.html { redirect_to @soccerfield, notice: 'Reserva actualizada con éxito.' }
         format.json { render :show, status: :ok, location: @soccerfield }
       else
         format.html { render :edit }
@@ -56,7 +63,7 @@ class SoccerfieldsController < ApplicationController
   def destroy
     @soccerfield.destroy
     respond_to do |format|
-      format.html { redirect_to soccerfields_url, notice: 'Soccerfield was successfully destroyed.' }
+      format.html { redirect_to soccerfields_url, notice: 'Reserva eliminada con éxito.' }
       format.json { head :no_content }
     end
   end
@@ -66,6 +73,10 @@ class SoccerfieldsController < ApplicationController
     def set_soccerfield
       @soccerfield = Soccerfield.find(params[:id])
     end
+
+    def set_user
+      @user = User.all
+    end    
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def soccerfield_params
