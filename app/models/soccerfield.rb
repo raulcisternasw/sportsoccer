94ff1346d0ec0da94c2a6bb54441598a3ce1ccf7
Raul Cisternas <r.cisternas.w@gmail.com>
2017-06-20 10:27:41 -0400

    ADD: Validate soccerfield

--- app/models/soccerfield.rb
+++ app/models/soccerfield.rb
@@ -1,5 +1,7 @@
 class Soccerfield < ApplicationRecord
   belongs_to :user
   validates :code, :state, :date, :time, :user_id, presence: true
+  validates_uniqueness_of :code, scope: [:state, :date, :time], message: 'Cancha ocupada en este horario, favor seleccione otro horario.'
+
   enum state: [ :'Sin reservar', :'Reservada' ]
 end
